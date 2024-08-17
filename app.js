538301abaa106842e77268646761de4aa0d2bda8
Nabeel Aslam <nabeel.azlam7@gmail.com>
2024-08-17 11:04:57 +0530

    first commit after adding public file and installing modules

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const env = require('dotenv').config()
+
+app.listen(process.env.PORT,()=>console.log("server running"))
+
+
+module.exports = app;
