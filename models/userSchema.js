1279cfd710b1c7eb49ace519620e779fd77ddef1
Nabeel Aslam <nabeel.azlam7@gmail.com>
2024-08-17 19:33:49 +0530

    front end template changed

---
+++ models/userSchema.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const userSchema = new Schema({
+    username:{
+        unique : true,
+        required : true
+    },
+    password:{
+        type : String,
+        required : true,
+        unique:true
+    },
+    email:{
+        type : String,
+        required : true,
+        unique : true
+    },
+    phoneNumber:{
+        type : String,
+        required : false,
+        unique : true,
+        sparse : true,
+        default:null
+    },
+    googleId:{
+        type : String,
+        unique : true
+    },
+    firstName:{
+        type : String,
+        required : false,
+        unique : false,
+        sparse : true,
+        default:null
+    },
+    lastName:{
+        type : String,
+        required : false,
+        unique : false,
+        sparse : true,
+        default:null
+    },
+    isActice:{
+        type:Boolean,
+        default : true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'], 
+        required: true
+      }
+})
