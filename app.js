277137a6405c8899b60fe061ff93e6196fff8607
Nabeel Aslam <nabeel.azlam7@gmail.com>
2024-08-17 12:42:01 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const env = require('dotenv').config()
-
+const port = process.env.PORT||3000;
 app.listen(process.env.PORT,()=>console.log("server running"))
 
 
