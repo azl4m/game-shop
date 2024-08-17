e9b6d9f24f262ad5ad2e97f852dff9f80b3226d4
Nabeel Aslam <nabeel.azlam7@gmail.com>
2024-08-17 20:51:21 +0530

    productSchema created

---
+++ models/productModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const variantSchema = new mongoose.Schema({
+    version: {
+      type: String,
+      required: true
+    },
+    platforms: {
+      type: [String], 
+      required: true,
+      enum: ['PC', 'PS5', 'PS6', 'Xbox One', 'Steam'] // List of allowed platforms
+    }
+  });
+
+const productSchema = new Schema({
+    productName : {
+        type : String,
+        required : true
+    },
+    description :{
+        type : String,
+        required : true,
+    },
+    images: {
+        type: [String], 
+        validate: {
+          validator: function(array) {
+            return array.length >= 3;
+          },
+          message: 'A product must have at least 3 images.'
+        },
+        required: true
+      },
+      variant: [variantSchema],
+      createdAt:{
+        type : Date,
+        default : Date.now
+      },
+      updatedAt : {
+        type : Date,
+        default : Date.now
+      },
+      createdBy :{
+        type : String,
+      },
+      updatedBy : {
+        type : String
+      }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
