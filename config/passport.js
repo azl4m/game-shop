f9b6a68f0e21024589bb0da75de45d6422ea95ca
Nabeel Aslam <nabeel.azlam7@gmail.com>
2024-11-08 15:20:29 +0530

    fixed google callback errore

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://thegameshop.shop/auth/google/callback",
       passReqToCallback: true,
     },
     async (req, accessToken, refereshToken, profile, done) => {
